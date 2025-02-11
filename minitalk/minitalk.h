83b285c0f51335d7ea2f3087760697330b8350ba
HOUSSAM MNASFA <126343598+MNASFA@users.noreply.github.com>
2025-02-11 11:37:29 +0100

    Add files via upload

---
+++ minitalk/minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmnasfa <hmnasfa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 10:02:47 by hmnasfa           #+#    #+#             */
+/*   Updated: 2025/02/11 10:18:57 by hmnasfa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+#endif
